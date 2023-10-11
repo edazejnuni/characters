535300f28e4d057449ba4c89f428871f4fa8198f
edazejnuni <>
2023-10-11 22:14:08 +0200

    fix login and homepage route

--- src/App.js
+++ src/App.js
@@ -1,57 +1,55 @@
 import React, { useEffect, useState } from 'react';
-import './App.css'
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import './App.css';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Header from './components/Header/Header';
-import Homepage from './pages/Homepage/Homepage'
+import Homepage from './pages/Homepage/Homepage';
 import Login from './pages/Login/Login';
 import AuthService from './services/AuthService';
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  useEffect(() => {
-    const checkAuthentication = async () => {
-      const token = AuthService.getToken();
-      const authenticated = !!token;
-      setIsAuthenticated(authenticated);
-      console.log(isAuthenticated)
+    useEffect(() => {
+        const checkAuthentication = async () => {
+            const token = AuthService.getToken();
+            const authenticated = !!token;
+            setIsAuthenticated(authenticated);
+        };
+
+        checkAuthentication();
+
+        window.addEventListener('storage', checkAuthentication);
+
+        return () => {
+            window.removeEventListener('storage', checkAuthentication);
+        }
+    }, []);
+
+    const handleLogout = () => {
+        AuthService.logout();
+        setIsAuthenticated(false);
     };
 
-    checkAuthentication();
-
-    window.addEventListener('storage', checkAuthentication);
-
-    return () => {
-      window.removeEventListener('storage', checkAuthentication);
-    }
-  }, []);
-  
-  const handleLogout = () => {
-    AuthService.logout();
-    setIsAuthenticated(false);
-  };
-
-  return (
-    <div className="App">
-      <Header isAuthenticated={isAuthenticated} handleLogout={handleLogout} />
-      <Router>
-        <Routes>
-          <Route path="/login" element={<Login setIsAuthenticated={setIsAuthenticated} />} />
-          <Route path="/" element={<Homepage />} />
-          <Route
-            path="/"
-            element={
-              !isAuthenticated ? (
-                <Navigate to="/login" replace />
-              ) : (
-                <Navigate to="/" replace />
-              )
-            }
-          />
-        </Routes>
-      </Router>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header isAuthenticated={isAuthenticated} handleLogout={handleLogout} />
+            <Router>
+                <Routes>
+                    <Route path="/login" element={<Login setIsAuthenticated={setIsAuthenticated} />} />
+                    <Route
+                        path="/"
+                        element={
+                            !isAuthenticated ? (
+                                <Navigate to="/login" replace />
+                            ) : (
+                                <Homepage />
+                            )
+                        }
+                    />
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
